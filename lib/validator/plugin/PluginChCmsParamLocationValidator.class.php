8dc2aab05c586a2710bdc3cda88d324a0101ba09
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-09-07 23:57:18 +0200

    added chCmsParamLocationValidator
    new file:   lib/validator/chCmsParamLocationValidator.class.php
    new file:   lib/validator/plugin/PluginChCmsParamLocationValidator.class.php
    new file:   test/unit/validator/chCmsParamLocationValidatorTest.php

---
+++ lib/validator/plugin/PluginChCmsParamLocationValidator.class.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This file declare the PluginChCmsParamLocationValidator class.
+ *
+ * @package chCmsApiPlugin
+ * @subpackage parma-validator
+ * @author Julien Muetton <julien_muetton@carpe-hora.com>
+ * @copyright (c) Carpe Hora SARL 2011
+ * @since 2011-09-04
+ */
+
+/**
+ * param validator
+ */
+class PluginChCmsParamLocationValidator extends chCmsValidatorApiBase
+{
+  /**
+   * configure the widget.
+   * add following options :
+   *  - min_length
+   * -  max_length
+   * add following messages:
+   * - invalid
+   * - server_error
+   * - max_length
+   * - min_length
+   *
+   * @param array $options  the validator options
+   * @param array $messages error messages
+   */
+  protected function configure($options = array(), $messages = array())
+  {
+    parent::configure($options, $messages);
+    $this->addoption('min_length', 3);
+    $this->addoption('max_length', 255);
+    $this->setMessage('invalid', 'Invalid data "%message%".');
+    $this->addMessage('server_error', 'geocoding server error "%message%".');
+    $this->addMessage('max_length', '"%value%" is too long (%max_length% characters max).');
+    $this->addMessage('min_length', '"%value%" is too short (%min_length% characters min).');
+  }
+
+  /**
+   * do clean
+   */
+  protected function doClean($value)
+  {
+    try
+    {
+      $validator = new sfValidatorString(array(
+        'min_length' => $this->getOption('min_length'),
+        'max_length' => $this->getOption('max_length')));
+
+      $value = $validator->clean($value);
+
+      try
+      {
+        // reverse geocode
+        $r = Geocoder::encode($value);
+
+        // extract first placemark
+        return array(
+          'lat'   => $r->Placemark[0]->Point->coordinates[1],
+          'long'  => $r->Placemark[0]->Point->coordinates[0]);
+      }
+      catch (sfException $e)
+      {
+        if (strstr($e->getMessage(), '602') || strstr($e->getMessage(), '603'))
+        {
+          throw new sfValidatorError($this, 'invalid', array('message' => $e->getMessage()));
+        }
+
+        throw new sfValidatorError($this, 'server_error', array('message' => $e->getMessage()));
+      }
+
+      return $value;
+    }
+    catch(sfValidatorError $e)
+    {
+      throw new sfValidatorError($this, $e->getCode(), $e->getArguments());
+    }
+  }
+} // END OF PluginChCmsParamLocationValidator

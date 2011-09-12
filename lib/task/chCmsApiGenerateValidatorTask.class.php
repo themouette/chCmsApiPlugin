0945a219a747ef94a3263dcf45549a28e206ccd0
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-09-12 12:20:04 +0200

    updated default validator class
    modified:   lib/task/chCmsApiGenerateValidatorTask.class.php

--- lib/task/chCmsApiGenerateValidatorTask.class.php
+++ lib/task/chCmsApiGenerateValidatorTask.class.php
@@ -21,7 +21,7 @@ class chCmsApiGenerateValidatorTask extends sfGeneratorBaseTask
     ));
 
     $this->addOptions(array(
-      new sfCommandOption('base-class', null, sfCommandOption::PARAMETER_REQUIRED, 'The base class to extend from', 'sfValidator'),
+      new sfCommandOption('base-class', null, sfCommandOption::PARAMETER_REQUIRED, 'The base class to extend from', 'chCmsValidatorApiBase'),
       new sfCommandOption('plugin', null, sfCommandOption::PARAMETER_REQUIRED, 'The plugin to generate class in', null),
     ));
 

23d3d1b006715f094db5263bf4ea51ed72335c1b
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-10-28 01:32:36 +0200

    add chCmsApiDateTimePropertyFormatter

---
+++ lib/formatter/plugin/PluginChCmsApiDateTimePropertyFormatter.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file declare the PluginChCmsApiDateTimePropertyFormatter class.
+ *
+ * @package chCmsApiPlugin
+ * @subpackage formatter
+ * @author Julien Muetton <julien_muetton@carpe-hora.com>
+ * @copyright (c) Carpe Hora SARL 2011
+ * @since 2011-09-04
+ */
+
+/**
+ * Class with no description
+ */
+class PluginChCmsApiDateTimePropertyFormatter extends chCmsApiPropertyFormatter
+{
+  /**
+   * constructor
+   *
+   * @return void
+   */
+  public function __construct($field, $options = array())
+  {
+    parent::__construct($field, array_merge(array(
+            'format'  => DateTime::ISO8601), $options));
+  }
+
+  /**
+   * format
+   *
+   * @return mixed
+   */
+  public function format($date)
+  {
+    $format = $this->getOption('format');
+    return parent::format($date)->format($format);
+  }
+} // END OF PluginChCmsApiDateTimePropertyFormatter
